7a0129d768615f8e90cb335de0127d826f03b393
Jonathan Gendu <jogendu@tsn.at>
2022-11-03 07:58:49 +0100

    Aufgabe2

---
+++ 2Aufgabe/Fakultät_Gendu_Jonathan/src/Fakultät.java
@@ -0,0 +1,30 @@
+public class Fakultät {
+
+    public static void main(String[] args) {
+        System.out.println(FakultätRe(3));
+        System.out.println(FakultätIn(3));
+    }
+
+    public static int FakultätRe(int Zahl){
+        if (Zahl <= 1){
+            return 1;
+        }
+        else{
+            return Zahl*FakultätRe(Zahl-1);
+        }
+    }
+
+    public static int FakultätIn(int Zahl){
+        int puffer = 1;
+        if (Zahl <=1){
+            return 1;
+        }
+        else {
+
+            for (int i = 1; i <= Zahl; i++) {
+                puffer = puffer * i;
+            }
+        }
+        return puffer;
+    }
+}
